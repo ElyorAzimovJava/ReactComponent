8294ded9c7e6ed910938ea934b5e266c5fc0efda
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-09-18 09:37:38 +0500

    Bank account with react reducer

--- reactPro/src/App.jsx
+++ reactPro/src/App.jsx
@@ -2,30 +2,33 @@ import Hello from "./component/Hello.jsx"
 import Fruits from "./component/Fruits.tsx"
 import Event from "./component/Event.jsx";
 import Counter from "./component/Counter.jsx";
-import {useReducer} from "react";
+import {useReducer, useState} from "react";
 
 function App() {
-   const [state,dispatch] = useReducer(reducer, {count: 0, incrementBy :1});
-   function reducer(state, action){
-    if(action.type === "increment"){
-        return  {...state, count : state.count + state.incrementBy};
+    const [isError, setIsError] = useState(false);
+    function reducer(state, action){
+      if(action.type === "add"){
+          return {...state, balance: state.balance + action.payload}
+       }
+      if(action.type === "subtract"){
+          if(state.balance < action.payload){
+              setIsError( true);
+              return state;
+          }
+          return {...state, balance: state.balance - action.payload}
+      }
     }
-    if(action.type === "decrement"){
-        return  {...state, count : state.count - state.incrementBy}
-    }
-    if(action.type === "setIncrement"){
-        return {...state, incrementBy : action.payload}
-    }
-   }
-  return <div className="App">
-
-
-      <h1>UseReducer {state.count}</h1>
-      <input value = {state.incrementBy}
-             onChange={(e) => dispatch({type: "setIncrement", payload : Number(e.target.value)})}/>
-      <button onClick ={()=> dispatch({type : "increment", payload : 1})} >Increment</button>
-      <button onClick ={()=> dispatch({type : "decrement", payload : 1})} >Decrement</button>
-         </div>
+    const [amount, setAmount] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {balance:0})
+  return (
+      <div>
+          <h1>Current balance {state.balance}</h1>
+          <input value = {amount} onChange={(e) => setAmount(Number(e.target.value))}/>
+          <button onClick={()=> dispatch({type:"add", payload: amount})}>Deposit</button>
+          <button onClick={()=> dispatch({type:"subtract", payload: amount})}>Withdraw</button>
+          {isError ? <h1 style={{color : "red"}}>Insufficient balance</h1> : ""}
+      </div>
+  )
 }
 
 export default App
