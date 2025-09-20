2b1c0219f843327e3dd89e501114bcd55d95c5af
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-09-20 08:27:43 +0500

    Nested routes in react

--- reactPro/src/App.jsx
+++ reactPro/src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from "./pages/HomePage.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import {Nav} from "./pages/Nav.jsx";
+import Profile from "./pages/Profile.jsx";
+import Setting from "./pages/Setting.jsx";
 
 function App() {
 
@@ -12,7 +14,10 @@ function App() {
           <Nav/>
           <Routes>
               <Route path = "/" element = {<HomePage/>}/>
-              <Route path = "dashboard" element = {<Dashboard/>}/>
+              <Route path = "dashboard" element = {<Dashboard/>}>
+                  <Route path = "profile" element = {<Profile/>}/>
+                  <Route path = "setting" element = {<Setting/>}/>
+              </Route>
               <Route path = "*" element = {<NotFound/>}/>
           </Routes>
       </Router>
---
+++ reactPro/src/pages/Setting.jsx
@@ -0,0 +1,7 @@
+export default function Setting(){
+    return(
+        <div>
+            This is a setting page
+        </div>
+    )
+}
